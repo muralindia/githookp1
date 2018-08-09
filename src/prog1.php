e2fbddfcb33178e68b46c4411606d80faddf08a5
Murali Dhandapani <muralindia@gmail.com>
2018-08-09 04:18:30 +0000

    updated the prog

--- src/prog1.php
+++ src/prog1.php
@@ -8,6 +8,13 @@
 <body>  
 
 <?php
+//comments
+
+$txt1 = "DevOps Demo using PHP";
+$txt2 = "Learnings!!";
+
+print "A day for " . $txt2 . "<br>";
+
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
