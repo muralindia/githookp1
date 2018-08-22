0545378cfa77a6178cdce3002a1e575dc27ba912
muralindia <muralindia@gmail.com>
2018-08-22 09:35:29 +0530

    Update prog1.php

--- src/prog1.php
+++ src/prog1.php
@@ -11,7 +11,7 @@
 //comments
 
 $txt1 = "DevOps Demo using PHP";
-$txt2 = "Learnings!!.. DevOps demo!!!.. Muralindia!!. Super!";
+$txt2 = "Learnings!!.. DevOps demo!!!.. Muralindia!!. Demo1 Super!";
 
 print "<h2>" ."A day for " .$txt2. "</h2>";
 
